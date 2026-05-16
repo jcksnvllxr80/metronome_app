98956ed1e995f45803b0078f726774b474f78b4d
Aaron Watkins <ac.watkins80@gmail.com>
2026-05-16 14:21:16 -0400

    Initial Commit

---
+++ meter-gnome/meter_gnomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  meter_gnomeApp.swift
+//  meter-gnome
+//
+//  Created by Aaron watkins on 5/16/26.
+//
+
+import SwiftUI
+
+@main
+struct meter_gnomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
